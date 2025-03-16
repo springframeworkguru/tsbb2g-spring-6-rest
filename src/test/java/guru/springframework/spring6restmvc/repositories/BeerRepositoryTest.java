3ffbc808cec3788ef21d4c3219ad8497235f7fd0
John Thompson <john@springframework.guru>
2025-03-16 11:42:25 -0400

    adding valid bean

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +20,14 @@ class BeerRepositoryTest {
     void testSave() {
         Beer beer = Beer.builder().build();
 
-        Beer savedBeer = beerRepository.save(beer);
+        beer.setBeerName("name");
+        beer.setBeerStyle(BeerStyle.LAGER);
+        beer.setUpc("123456789012");
+        beer.setPrice(new BigDecimal("12.99"));
+
+
+        Beer savedBeer = beerRepository.saveAndFlush(beer);
+        //beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
 
